b29bc483f7a672450dc732831231c7cd001dc1f9
Dmitry Golub <dmgolub@mail.ru>
2023-01-07 01:40:11 +0400

    Task completed.

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,12 @@
 package com.epam.utils;
 
 public class StringUtils {
+
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            return Integer.parseInt(str) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
